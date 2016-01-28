d770b8ffda6faf055f0160677048b36fa5ea88d7
jared beckler and none <student@epicodus.com>
2016-01-28 08:18:30 -0800

    add initial specs. Github down yesterday

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 var animalArray = ["tiger","giraffe","chimpanzee","elephant","squid"];
 var cityArray = ["Spokane", "McKinney", "Portland", "Chicago", "Columbus"];
 
-
 function Hangman(category) {
   this.category = category;
 }
@@ -13,22 +12,44 @@ Hangman.prototype.chooseWord = function(userInput) {
   } else {
     return (animalArray[random]).split("");
   };
+  /** for ease, will call the output of this function randomArray **/
 };
 
-var emptyArray = function(word) {
-  var blankArray = word.slice();
-  return blankArray;
+var copiedArray = function(randomArray) {
+  randomArray = randomArray.join("");
+  var copiedArray = randomArray.split("");
+  return(copiedArray)
+}
+
+var copiedBlankArray = function(randomArray) {
+  var copiedBlankArray = randomArray.slice();
+  for (var i = 0; i < copiedBlankArray.length; i++) {
+    copiedBlankArray[i] = "";
+  };
+  return copiedBlankArray;
 };
 
-
-
-
+var wrongRightLetter = function(letter, copiedArray, copiedBlankArray, wrongArray) {
+  if (wrongArray.length > 5) {
+    return "fail";
+  } else if (copiedBlankArray === copiedArray) {
+    return "you win";
+  } else if (copiedArray.indexOf(letter) === -1) {
+    wrongArray.push("x");
+    return wrongArray;
+  } else {
+    var index = copiedArray.indexOf(letter);
+        copiedBlankArray[index] = letter;
+        return copiedBlankArray;
+  };
+};
 
 $(document).ready(function() {
   var gameCities = new Hangman("cities");
   var gameAnimals = new Hangman("animals");
   var chooseAnimals = $("#animals").val();
   var chooseCities = $("#cities").val();
+  var wrongArray = [];
 
   $("form").submit(function() {
     event.preventDefault();
--- specs/specs.js
+++ specs/specs.js
@@ -4,7 +4,47 @@ describe("Hangman", function() {
     expect(newGame.chooseWord()).to.be.a('array');
   });
 
-  it("creates a copy of original array", function() {
-    expect(newGame.chooseWord().length).to.be.a('number');
+  it("creates an exact copy of the array and split it", function() {
+    var randomArray = ["hi"]
+    expect(copiedArray(randomArray)).to.eql(["h", "i"])
   });
+
+  it("creates a blank array that's the same length as selected array", function() {
+    var testArray = [1,2,3]
+    expect(copiedBlankArray(testArray)).to.eql(["","",""]);
+  });
+
+  it("check whether a letter is wrong and pushes it to wrong array", function() {
+    var copiedArray = ["t","i","g","e","r"];
+    var letter = "q"
+    var wrongArray = [];
+    expect(wrongRightLetter(letter, copiedArray, copiedBlankArray, wrongArray)).to.eql(["x"]);
+  })
+
+  it("check whether a letter is right and pushes it to copiedBlankArray", function() {
+    var copiedArray = ["t","i","g","e","r"];
+    var letter = "g";
+    var copiedBlankArray = ["","","","",""];
+    var wrongArray = ["x","x","x","x","x"];
+    expect(wrongRightLetter(letter, copiedArray, copiedBlankArray, wrongArray)).to.eql(["","","g","",""]);
+  });
+
+  it("notifies you when you've lost the game",function() {
+    var wrongArray = ["x","x","x","x","x","x"];
+    var letter = "q";
+    var copiedBlankArray = ["","","","",""];
+    var copiedArray = ["t","i","g","e","r"];
+    expect(wrongRightLetter(letter, copiedArray, copiedBlankArray, wrongArray)).to.equal("fail");
+  });
+
+
+/** NOT FINISHED **/
+  it("check for duplicate letters", function() {
+    var wrongArray = [];
+    var letter = "p";
+    var copiedBlankArray = ["","","","",""];
+    var copiedArray = ["a","p","p","l","e"];
+    expect(wrongRightLetter(letter, copiedArray, copiedBlankArray, wrongArray)).to.eql(["","p","p","",""]);
+  });
+
 });
